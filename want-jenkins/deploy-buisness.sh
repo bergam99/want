09e312a2c9677e0f2a9486d917c6807e493398a4
Jiyun <grosjeanjiyun@icloud.com>
2025-11-11 14:02:04 +0100

    back deployment

--- want-jenkins/deploy-buisness.sh
+++ want-jenkins/deploy-buisness.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# set -e # Failure et arreter à l'erreur
+# set -e
 
 readonly SCRIPT=$0
-readonly TOTO_BUISNESS_DEPLOY_DIR=$1 # deposer .jar
-readonly TOTO_BUISNESS_URL=$2
-readonly TOTO_BUISNESS_PORT=$3
+readonly WANT_BUISNESS_DEPLOY_DIR=$1 # deposer .jar
+readonly WANT_BUISNESS_URL=$2
+readonly WANT_BUISNESS_PORT=$3
 
 echo "> Execution ${SCRIPT}"
 
-echo "> Stopping toto-buisness (optional operation)"
+echo "> Stopping want-buisness (optional operation)"
 
 # check if a process exists on port:
-lsof -ti tcp:$TOTO_BUISNESS_PORT #recuperer le port en variable au niveau propriete folder
+lsof -ti tcp:$WANT_BUISNESS_PORT #recuperer le port en variable au niveau propriete folder
 readonly LSOF_EXIT_CODE=$?
 
 # Stop if script execution failed
@@ -21,7 +21,7 @@ set -e # pas tout en haut si pid exists pas
 # si ya seulement pas de process en cours alors demarre
 if [ $LSOF_EXIT_CODE -eq 0 ]; then
     # Get PID (process id)
-    readonly PID=$(lsof -ti tcp:$TOTO_BUISNESS_PORT)
+    readonly PID=$(lsof -ti tcp:$WANT_BUISNESS_PORT)
     echo "killing gracefully the process with PID=${PID}..."
     kill -15 $PID
     echo " > Process killed"
@@ -30,13 +30,12 @@ else
 fi
 
 
-echo " > Deploy target/*.jar to ${TOTO_BUISNESS_DEPLOY_DIR}"
-rm -rf $TOTO_BUISNESS_DEPLOY_DIR/* # supprimer dans le folder l'ancien build
-cp ./toto-buisness/target/*.jar $TOTO_BUISNESS_DEPLOY_DIR/toto-buisness.jar # sur ecrire (les artifacts de build dans dist -> copy deploy directory)
+echo " > Deploy target/*.jar to ${WANT_BUISNESS_DEPLOY_DIR}"
+rm -rf $WANT_BUISNESS_DEPLOY_DIR/* # supprimer dans le folder l'ancien build
+cp ./want-buisness/target/*.jar $WANT_BUISNESS_DEPLOY_DIR/want-buisness.jar # sur ecrire (les artifacts de build dans dist -> copy deploy directory)
 
-# no hang up
-JENKINS_NODE_COOKIE=dontKillMe nohup java -jar -Dspring.profiles.active=prod $TOTO_BUISNESS_DEPLOY_DIR/toto-buisness.jar &
+JENKINS_NODE_COOKIE=dontKillMe nohup java -jar -Dspring.profiles.active=prod $WANT_BUISNESS_DEPLOY_DIR/want-buisness.jar &
 
 echo " > Deployed !!"
-echo $TOTO_BUISNESS_URL
+echo $WANT_BUISNESS_URL
 
--- want-jenkins/prepare-buisness.sh
+++ want-jenkins/prepare-buisness.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-set -ex # Failure et arreter à l'erreur
+set -e
 
-# $0 = script meme
-# declarer constant
 readonly SCRIPT=$0
 readonly ENV_FILE_PATH=$1
 
@@ -11,13 +9,11 @@ echo "> Execution ${SCRIPT}"
 
 echo " > Preparing buisness artifacts with maven and java: "
 
-# application-prod.properties
-echo " > Copy tmp env file to ./toto-buisness/src/main/resources/application-prod.properties"
-cp $ENV_FILE_PATH ./toto-buisness/src/main/resources/application-prod.properties # copie source -> destination
+echo " > Copy tmp env file to ./want-buisness/src/main/resources/application-prod.properties"
+cp $ENV_FILE_PATH ./want-buisness/src/main/resources/application-prod.properties # copie source -> destination
 
 mvn -v
-mvn -Dmaven.test.skip=true -f ./toto-buisness/pom.xml package
+mvn -Dmaven.test.skip=true -f ./want-buisness/pom.xml package
 
-# application-prod.properties
-echo " > Copy tmp env file to ./toto-buisness/.env.production"
-cp $ENV_FILE_PATH ./toto-presentation/.env.production # copie source -> destination
+echo " > Copy tmp env file to ./want-buisness/.env.production"
+cp $ENV_FILE_PATH ./want-presentation/.env.production
