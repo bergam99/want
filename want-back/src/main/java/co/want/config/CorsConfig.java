c4e8d1b1c9caad93d1211e39a666a639277ec29d
Jiyun <grosjeanjiyun@icloud.com>
2025-12-14 12:50:07 +0100

    deploiment

--- want-back/src/main/java/co/want/config/CorsConfig.java
+++ want-back/src/main/java/co/want/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package co.want.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+	
+
+    @Value("${want.cors.allowedOrigins}")
+    private String allowedOrigins;
+    
 	@Bean
 	WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**") // about all endpoints
-						.allowedOrigins("http://localhost:5173") // allow only "http://localhost:5173"
+						.allowedOrigins(allowedOrigins)
 						.allowedMethods("GET", "POST", "PUT", "DELETE") // allow only these HTTP methods
 						.allowCredentials(true); // allow origin contain Authorization header or cookie
 			}
