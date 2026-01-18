2bb3288fecd2b5edd65f62627d64c94cba6a6039
Jiyun <grosjeanjiyun@icloud.com>
2026-01-18 23:13:38 +0100

    test

--- want-back/src/test/java/co/want/controllers/ReviewControllerIT.java
+++ want-back/src/test/java/co/want/controllers/ReviewControllerIT.java
@@ -10,25 +10,20 @@ import org.junit.jupiter.api.Test;
 import co.want.BaseIT;
 
 public class ReviewControllerIT extends BaseIT{
-	
 	@Test
-    void should_return_all_reviews() throws Exception {
-        final var builder = newGetRequestBuilder("/review/read/123456789");
-        
-        api.perform(builder)
-           .andExpect(status().isOk())
-           .andExpect(jsonPath("$.length()", is(2)))
-           
-           .andExpect(jsonPath("$[0].osmId", is(123456789)))
-           .andExpect(jsonPath("$[0].userEmail", is("alice@example.com"))) 
-           .andExpect(jsonPath("$[0].comment", is("content1")))           
-           .andExpect(jsonPath("$[0].rating", is(5)))
-           .andExpect(jsonPath("$[0].timeStamp").value(org.hamcrest.Matchers.containsString("2025-12-07T13:05:52")))
-           
-           .andExpect(jsonPath("$[1].osmId", is(123456789)))
-           .andExpect(jsonPath("$[1].userEmail", is("bob@example.com")))  
-           .andExpect(jsonPath("$[1].comment", is("content2")))          
-           .andExpect(jsonPath("$[1].rating", is(4)))
-           .andExpect(jsonPath("$[1].timeStamp").value(org.hamcrest.Matchers.containsString("2025-12-07T13:08:10")));
-    }
+	void should_return_all_reviews() throws Exception {
+	    final var builder = newGetRequestBuilder("/review/read/123456789");
+	    
+	    api.perform(builder)
+	       .andExpect(status().isOk())
+	       .andExpect(jsonPath("$.length()", is(2)))
+	       
+	       .andExpect(jsonPath("$[0].userEmail", is("alice@example.com"))) 
+	       .andExpect(jsonPath("$[0].comment", is("content1")))           
+	       .andExpect(jsonPath("$[0].timeStamp").value(org.hamcrest.Matchers.containsString("2025-12-07T12:05:52")))
+	       
+	       .andExpect(jsonPath("$[1].userEmail", is("bob@example.com")))   
+	       .andExpect(jsonPath("$[1].comment", is("content2")))           
+	       .andExpect(jsonPath("$[1].timeStamp").value(org.hamcrest.Matchers.containsString("2025-12-07T12:08:10")));
+	}
 }
