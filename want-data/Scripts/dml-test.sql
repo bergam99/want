65fdc8bd78915d151738fa0fec27b27b1bfd0aeb
Jiyun <grosjeanjiyun@icloud.com>
2026-01-18 23:10:06 +0100

    test

--- want-data/Scripts/dml-test.sql
+++ want-data/Scripts/dml-test.sql
@@ -8,9 +8,9 @@ INSERT INTO want_test.t_users (email, password) VALUES
 ('charlie@example.com', '$2a$10$zyxwvutsrqponmlkjihgfedcba654321');
 
 INSERT INTO want_test.t_reviews (osm_id, user_id, comment, rating, time_stamp, like_count) VALUES
-(123456789, 1, 'content1', 5, '2025-12-07 13:05:52', 2),
-(123456789, 2, 'content2', 4, '2025-12-07 13:08:10', 1),
-(987654321, 1, 'content3', 2, '2025-12-07 15:00:00', 0);
+(123456789, 1, 'content1', 5, '2025-12-07 12:05:52+00', 2),
+(123456789, 2, 'content2', 4, '2025-12-07 12:08:10+00', 1),
+(987654321, 1, 'content3', 2, '2025-12-07 14:00:00+00', 0);
 
 INSERT INTO want_test.t_review_likes (review_id, user_id) VALUES
 (1, 2), 
